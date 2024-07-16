ee7953982c058a83903e0439bde9e1775c10d8f4
DUB1401 <vlad.milosta@outlook.com>
2024-07-16 20:05:15 +0300

    Release 2.924

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 flatpak install flathub org.flatpak.Builder -y
 flatpak install flathub org.freedesktop.Sdk/x86_64/23.08 -y
 flatpak install flathub org.freedesktop.Sdk.Extension.openjdk21/x86_64/23.08 -y
-flatpak run org.flatpak.Builder --install --user build org.tlauncher.TLauncher.json
+flatpak run org.flatpak.Builder build org.tlauncher.TLauncher.json
+flatpak build-export export build
+flatpak build-bundle export TLauncher.flatpak org.tlauncher.TLauncher
