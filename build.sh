369411dd6755a10818054f8628de4d0e3cb758f9
DUB1401 <vlad.milosta@outlook.com>
2024-07-16 21:08:47 +0300

    Add: AppStream note

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ flatpak install flathub org.freedesktop.Sdk/x86_64/23.08 -y
 flatpak install flathub org.freedesktop.Sdk.Extension.openjdk21/x86_64/23.08 -y
 flatpak run org.flatpak.Builder build org.tlauncher.TLauncher.json
 flatpak build-export export build
-flatpak build-bundle export TLauncher.flatpak org.tlauncher.TLauncher
+flatpak build-bundle export TLauncher.flatpak org.tlauncher.TLauncher --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
